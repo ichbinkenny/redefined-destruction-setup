f2ba640e8589e784130656de5687ec46784b422d
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-17 09:04:48 +0100

    Updated libnfc reference as package name has changed.

--- initSystem.sh
+++ initSystem.sh
@@ -5,4 +5,4 @@ sudo apt-get upgrade
 sudo apt-get install bluez python3-pip libbluetooth-dev
 
 sudo pip3 install pybluez
-sudo apt-get install libnfc libfreefare-dev
+sudo apt-get install libnfc-dev libnfc-bin libfreefare-dev
