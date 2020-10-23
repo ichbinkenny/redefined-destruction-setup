9833c586ccb7029fbcc38717ca1cfa90bbcdee90
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-23 19:39:34 +0100

    Added dbus configs to ensure pi bluetooth properly communicates with Python.

--- initSystem.sh
+++ initSystem.sh
@@ -9,6 +9,11 @@ sudo apt-get install libnfc-dev libnfc-bin libfreefare-dev
 
 # Modifications to allow pybluez to function properly in compatability mode
 echo "Setting up system dbus files..."
-sudo cp -f ./dbus-org.bluez.service /etc/systemd/system/
+sudo cp -f /home/pi/redefined-destruction/Setup/dbus-org.bluez.service /etc/systemd/system/
+sudo cp -f /home/pi/redefined-destruction/Setup/dbus-org.bluez.service /lib/systemd/system/bluetooth.service
 sudo sdptool add SP
+sudo systemctl daemon-reload
+sudo systemctl restart bluetooth
+sudo hciconfig hci0 up
+sudo hciconfig hci0 piscan
 echo "Complete!"
