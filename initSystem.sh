2e6c3ea085c79cd0810e4eeb664b113b38685162
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-23 16:27:28 +0100

    Updated setup to provide bluez fix for python code.

--- initSystem.sh
+++ initSystem.sh
@@ -6,3 +6,9 @@ sudo apt-get install bluez python3-pip libbluetooth-dev
 
 sudo pip3 install pybluez
 sudo apt-get install libnfc-dev libnfc-bin libfreefare-dev
+
+# Modifications to allow pybluez to function properly in compatability mode
+echo "Setting up system dbus files..."
+sudo cp -f ./dbus-org.bluez.service /etc/systemd/system/
+sudo sdptool add SP
+echo "Complete!"
