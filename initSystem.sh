20465a1b705ab579a9f7f5df6e310b23330dd836
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-23 23:07:30 +0100

    HostAPD for server setup is now automated.

--- initSystem.sh
+++ initSystem.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
+printf "Will this device act as the server? [Y/n]: "
+
+read isServer
+
+ssid="Battle Bot Server"
+pass="123456789"
+
+if [ $isServer != "Y" ] && [ $isServer != "y" ]
+then
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install bluez python3-pip libbluetooth-dev
+sudo apt-get -y upgrade
+sudo apt-get -y install bluez python3-pip libbluetooth-dev
 
 sudo pip3 install pybluez
-sudo apt-get install libnfc-dev libnfc-bin libfreefare-dev
+sudo apt-get -y install libnfc-dev libnfc-bin libfreefare-dev
 
 # Modifications to allow pybluez to function properly in compatability mode
 echo "Setting up system dbus files..."
@@ -24,3 +33,50 @@ echo "Enabling System Services"
 sudo systemctl enable bluetooth_start.service
 sudo systemctl start bluetooth_start.service
 echo "Complete!"
+
+else
+    echo "Installing server software..."
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt-get -y install -y hostapd dnsmasq
+
+    printf "Would you like to enter a custom ssid? [Y/n]: "
+    read ans
+    if [ $ans = "Y" ] || [ $ans = "y" ]
+    then
+        printf "Please enter the desired SSID: "
+        read ssid
+    fi
+
+    printf "Would you like to enter a custom password? [Y/n]: "
+    read ans
+    if [ $ans = "y" ] || [ $ans = "Y"]
+    then
+        printf "Please enter the server password: "
+        read pass
+    fi
+
+    ipv4_update=$(awk '/^net.ipv4.ip_forward=/{print}' /etc/sysctl.conf)
+    
+    if [ $ipv4_update = "" ]
+    then
+        echo "setting up nat forwarding"
+        sudo echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+    fi
+
+    iptbl=$(awk '/^iptables -t nat -A POSTROUTING -s 192.168.8.0/24 ! -d 192.168.8.0/24  -j MASQUERADE/{print}' /etc/rc.local)
+
+    if [ $iptbl = "" ]
+    then
+        echo "adding iptables rules"
+        sudo echo "iptables -t nat -A POSTROUTING -s 192.168.8.0/24 ! -d 192.168.8.0/24  -j MASQUERADE" >> /etc/rc.local
+    fi
+
+    echo "Server is setup as $ssid with password $pass"
+    printf "interface=wlan0\nssid=$ssid\nhw_mode=n\nwpa=2\nwpa_passphrase=$pass\nwpa_key_mgmt=WPA-PSK WPA-EAP WPA-PSK-SHA256 WPA-EAP-SHA256\n" > hostapd.conf
+
+    echo "Copying DNSMASQ config"
+    sudo cp dnsmasq.conf /etc/
+
+    sudo cp hostapd.conf /etc/hostapd/
+fi
